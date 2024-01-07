c17ada19a507cb94258136f7f54dfde29365dc6b
Mavri <me@mavri.dev>
2024-01-07 17:48:04 +0200

    feat: added team members in the about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,9 +2,27 @@ import { title } from "@/components/primitives";
 
 export default function AboutPage() {
   return (
-    <div>
-      <h1 className={title()}>About</h1>
-      {/* TODO: Team that worked on this */}
-    </div>
+    <>
+      <div>
+        <h1 className={title()}>Participanti</h1>
+      </div>
+
+      <div className="py-3">
+        Radu-Leonard Mavrodin <br></br>
+        <li>Backend - Typescript, Node.js, Express.js, TypeORM, Zod</li>
+      </div>
+      <div className="py-3">
+        Octavian Eduard Pantazi <br></br>
+        <li>Database - Postgres, Hosting</li>
+      </div>
+      <div className="py-3">
+        Lupascu Mihaita Eduard <br></br>
+        <li>Frontend - Typescript, Next.js</li>
+      </div>
+      <div className="py-3">
+        Lupu Miruna Cristina <br></br>
+        <li>Design Frontend - NextUI, Figma</li>
+      </div>
+    </>
   );
 }
